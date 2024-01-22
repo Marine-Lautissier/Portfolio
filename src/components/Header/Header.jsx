76968b5de75abbca324043fd1fabbcb0b0145477
unknown <Marine-L@outlook.com>
2024-01-22 15:41:42 +0100

    updating component Header with useTypewriter

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,23 @@
 import './Header.css'
 import image from "../../assets/growtika-unsplash.jpg"
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+
 function Header() {
+    const [text] = useTypewriter({
+        words: ['Bienvenue sur mon Portfolio !'],
+        loop: 0,
+        typeSpeed: 120,
+        deleteSpeed: 80,
+    });
+
     return (
         <header className="introduction-section" style={{ backgroundImage: `url(${image})` }}>
-            <h2>Bienvenue sur mon Portfolio !</h2>
+            <h2> {text} <Cursor /> </h2>
             <div className="align-center">
             <p>Je me prénomme Marine Lautissier,</p>
             <p>Développeuse web / web mobile </p>
             <p>Full Stack - spécialisée en <strong className='Policy-React'>React</strong>.</p>
             </div>
-
         </header>
     )
 }
