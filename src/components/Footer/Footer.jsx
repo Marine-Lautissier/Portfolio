0117e1fe67b3329ec584dc4703ccaa902f807ebb
unknown <Marine-L@outlook.com>
2024-02-02 22:56:52 +0100

    updating component Header: modification CSS, and updating component Footer: Github link and Mail link added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,9 +9,9 @@ function Footer() {
             <div className="foot-links">
                 <ul>
                     <div className="footer-element">
-                        <li><a className= "button_foot" href="#">Mail</a></li>
+                        <li><a className= "button_foot" href="mailto:marine.lautissier@protonmail.com">Mail</a></li>
                         <div className="socialIcons">
-                        <a href=""><Mail /></a>
+                        <a href="mailto:marine.lautissier@protonmail.com"><Mail /></a>
                         </div>
                     </div>
 
@@ -23,9 +23,9 @@ function Footer() {
                     </div>
 
                     <div className="footer-element">
-                        <li><a className= "button_foot" href="#">Github</a></li>
+                        <li><a className= "button_foot" href="https://github.com/Marine-Lautissier">Github</a></li>
                         <div className="socialIcons">
-                            <a href=""><GitHub /></a>
+                            <a href="https://github.com/Marine-Lautissier"><GitHub /></a>
                         </div>
                     </div>
                 </ul>
